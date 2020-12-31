08ce45d19585721d054c47046943ab23e9fc675a
Matthew Gerring <matthew.gerring@jax.org>
2020-12-31 10:11:04 +0000

    Boot files

--- boot.sh
+++ boot.sh
@@ -3,4 +3,4 @@
 # Or create ~/.config/lxsession/LXDE-pi/ and copy autostart there
 # Put in:
 #     @lxterminal -e "/home/pi/raspberry-pi-devastator-robot/boot.sh"
-/usr/bin/python /home/pi/raspberry-pi-devastator-robot/src/robot.py &
+/usr/bin/python /home/pi/raspberry-pi-devastator-robot/src/robot.py 
