5e4ab9486b187868e59a97aec12dd67e134fad1a
Matthew Gerring <matthew.gerring@jax.org>
2021-01-04 15:31:53 +0000

    Update motor script

--- src/robot.py
+++ src/robot.py
@@ -6,17 +6,19 @@ import time #import time module
 
 #set GPIO numbering mode and define output pins
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7,GPIO.OUT)
-GPIO.setup(11,GPIO.OUT)
+
+# 16 and 18 are not on by defaut.
+GPIO.setup(16,GPIO.OUT) # 7 in his
+GPIO.setup(18,GPIO.OUT) # 11 in his
+
+# Keep 13 and 15 the same
 GPIO.setup(13,GPIO.OUT)
 GPIO.setup(15,GPIO.OUT)
-GPIO.setup(29,GPIO.OUT)
 
-for x in range(1, 10):
-    GPIO.output(29,False)
-    time.sleep(.5)
-    GPIO.output(29,True)
-    time.sleep(1)
+# LED
+GPIO.setup(29,GPIO.OUT)
+GPIO.output(29,True)
+
 
 # Get the curses window, turn off echoing of keyboard to screen, turn on
 # instant (no waiting) key response, and use special values for cursor keys
@@ -33,28 +35,28 @@ try:
         if char == ord('S'): # Added for shutdown on capital S
             os.system ('sudo shutdown now') # shutdown right now!
         elif char == curses.KEY_UP:
-            GPIO.output(7,False)
-            GPIO.output(11,True)
+            GPIO.output(16,False)
+            GPIO.output(18,True)
             GPIO.output(13,False)
             GPIO.output(15,True)
         elif char == curses.KEY_DOWN:
-            GPIO.output(7,True)
-            GPIO.output(11,False)
+            GPIO.output(16,True)
+            GPIO.output(18,False)
             GPIO.output(13,True)
             GPIO.output(15,False)
         elif char == curses.KEY_RIGHT:
-            GPIO.output(7,True)
-            GPIO.output(11,False)
-            GPIO.output(13,False)
-            GPIO.output(15,True)
-        elif char == curses.KEY_LEFT:
-            GPIO.output(7,False)
-            GPIO.output(11,True)
+            GPIO.output(16,False)
+            GPIO.output(18,True)
             GPIO.output(13,True)
             GPIO.output(15,False)
+        elif char == curses.KEY_LEFT:
+            GPIO.output(16,True)
+            GPIO.output(18,False)
+            GPIO.output(13,False)
+            GPIO.output(15,True)
         elif char == 10:
-            GPIO.output(7,False)
-            GPIO.output(11,False)
+            GPIO.output(16,False)
+            GPIO.output(18,False)
             GPIO.output(13,False)
             GPIO.output(15,False)
              
